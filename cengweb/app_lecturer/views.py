0b82657a6165e48ce7a1c854f39679e94bf1575a
ozankahramanoglu <ozankahramanoglu96@gmail.com>
2017-12-09 22:20:07 +0300

    Lecturer url is handled

--- cengweb/app_lecturer/views.py
+++ cengweb/app_lecturer/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Lecturer
 
 
 def index(request):
     context = {"asd": "asd"}
-    return render(request, 'app_lecturer/index.html', context)
+    return render(request, 'lecturer/index.html', context)
+
+
+def lecturers(request):
+    lecturer = Lecturer.objects.all()
+    context = {"lecturer": lecturer}
+    return render(request, 'index.html', context)
