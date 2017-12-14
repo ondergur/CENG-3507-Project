65e59cf1eb91354967d1352e5a624b79107135f7
ozankahramanoglu <ozankahramanoglu96@gmail.com>
2017-12-14 18:49:37 +0300

    second, third and fourth class added

--- cengweb/app_lecture/urls.py
+++ cengweb/app_lecture/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^firstclass/', views.classone, name="firstclass"),
-    url(r'^secondclass/', views.classone, name="secondclass"),
-    url(r'^thirdclass/', views.classone, name="thirdclass"),
-    url(r'^fourthclass/', views.classone, name="fourthclass"),
+    url(r'^secondclass/', views.classtwo, name="secondclass"),
+    url(r'^thirdclass/', views.classthree, name="thirdclass"),
+    url(r'^fourthclass/', views.classfour, name="fourthclass"),
 ]
