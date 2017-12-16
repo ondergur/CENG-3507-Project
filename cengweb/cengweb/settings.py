7c4d90745368db6dcab742e49c7308d0628f415e
ondergur <ahmetondergur195@gmail.com>
2017-12-16 13:38:26 +0300

    new app: app_lab

--- cengweb/cengweb/settings.py
+++ cengweb/cengweb/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app_lab.apps.AppLabConfig'
     'app_lecture.apps.AppLectureConfig',
     'app_lecturer.apps.AppLecturerConfig',
     'django.contrib.admin',
