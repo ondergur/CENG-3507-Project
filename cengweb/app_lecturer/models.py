59dc8773d7ef17873271ba6d3f5c775edc3fbe17
Ahmet Önder Gür <ahmetondergur195@gmail.com>
2017-12-18 13:00:50 +0300

    changes in lecturers

--- cengweb/app_lecturer/models.py
+++ cengweb/app_lecturer/models.py
@@ -5,10 +5,14 @@ from django.db import models
 class Lecturer(models.Model):
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100, primary_key=True)
+    title = models.CharField(max_length=100, blank=True, null=True)
+    bolum = models.CharField(max_length=100, blank=True, null=True)
     office_code = models.CharField(max_length=20)
     e_mail = models.EmailField(max_length=100)
     telephone = models.CharField(max_length=15)
     web_site = models.TextField(validators=[URLValidator()])
+    aciklama = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to="uploads", blank=True, null=True)
 
     def __str__(self):
         return self.name + " " + self.surname
